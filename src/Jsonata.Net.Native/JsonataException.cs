78d995cca280eb15d3115fcb2092be44eeb1d356
mikhail-barg <mikhail.barg@gmail.com>
2021-10-13 00:51:18 +0300

    some support for creating objects tests: 400/1611

---
+++ src/Jsonata.Net.Native/JsonataException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jsonata.Net.Native
+{
+    public sealed class JsonataException: Exception
+    {
+        public string Code { get; }
+
+        public JsonataException(string code, string message)
+            : base($"{code}: {message}")
+        {
+            this.Code = code;
+        }
+    }
+}
