e009064a10c91a63a044053dc63d009cdb785de2
mikhail-barg <mikhail.barg@gmail.com>
2021-10-17 03:20:54 +0300

    added boolean() and not() functions Outcomes    521 Passed    607 Failed    483 Skipped

--- src/Jsonata.Net.Native/Eval/BuiltinFunctions.cs
+++ src/Jsonata.Net.Native/Eval/BuiltinFunctions.cs
@@ -9,6 +9,7 @@ namespace Jsonata.Net.Native.Eval
 {
     internal static class BuiltinFunctions
     {
+        #region Numeric aggregation functions
         /**
          Signature: $sum(array)
          Returns the arithmetic sum of an array of numbers. It is an error if the input array contains an item which isn't a number.
@@ -52,5 +53,75 @@ namespace Jsonata.Net.Native.Eval
                 return new JValue(result);
             }
         }
+        #endregion
+
+        #region Boolean functions
+        /**
+         Signature: $boolean(arg)
+         Casts the argument to a Boolean using the following rules: http://docs.jsonata.org/boolean-functions#boolean 
+         */
+        public static JToken boolean(JToken arg)
+        {
+            switch (arg.Type)
+            {
+            case JTokenType.Boolean:    
+                //Boolean:	unchanged
+                return arg;
+            case JTokenType.String:
+                //string: empty   false
+                //string: non-empty   true
+                return ((string)arg!) != "";
+            case JTokenType.Integer:
+                //number: 0	false
+                //number: non-zero    true
+                return ((long)arg) != 0;
+            case JTokenType.Float:
+                //number: 0	false
+                //number: non-zero    true
+                return ((double)arg) != 0;
+            case JTokenType.Null:
+                //null	false
+                return false;
+            case JTokenType.Array:
+                //array: empty	false
+                //array: contains a member that casts to true true
+                //array: all members cast to false    false
+                foreach (JToken child in arg.Children())
+                {
+                    JToken childRes = BuiltinFunctions.boolean(child);
+                    if (childRes.Type == JTokenType.Boolean && (bool)childRes)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            case JTokenType.Object:
+                //object: empty   false
+                //object: non-empty   true
+                return arg.HasValues;
+            case FunctionToken.TYPE:
+                //function	false
+                return false;
+            case JTokenType.Undefined:
+                return arg;
+            default:
+                throw new ArgumentException("Unexpected arg type: " + arg.Type);
+            }
+        }
+
+        /**
+         Signature: $not(arg)
+         Returns Boolean NOT on the argument. arg is first cast to a boolean
+         */
+        public static JToken not(JToken arg)
+        {
+            arg = BuiltinFunctions.boolean(arg);
+            if (arg.Type == JTokenType.Undefined)
+            {
+                return arg;
+            }
+            return !(bool)arg;
+        }
+        #endregion
     }
 }
