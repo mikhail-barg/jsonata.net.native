49ae2e3b591cd2bfa8245f28cc6d9d604d2db045
mikhail.barg <mikhail.barg@gmail.com>
2025-10-11 22:27:08 +0300

    added js-sample debug env

---
+++ misc/js-sample/app.js
@@ -0,0 +1,16 @@
+const jsonata = require('jsonata');
+
+const data = {
+    example: [
+        {value: 4},
+        {value: 7},
+        {value: 13}
+    ]
+};
+
+(async () => {
+    const expression = jsonata('$sum(example.value)');
+    const result = await expression.evaluate(data);  // returns 24
+    console.log(result);
+})()
+
